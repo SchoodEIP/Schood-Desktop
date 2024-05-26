a96fdc22ad592376315400cce1c5f7e677860202
Mateo Deroche <mateo.deroche@epitech.eu>
2024-05-27 01:01:43 +0200

    [~] add profile pic

--- src/routes/shared/ProfilePage.py
+++ src/routes/shared/ProfilePage.py
@@ -1,8 +1,56 @@
-from PySide6 import QtWidgets, QtCore, QtGui
+import base64
+
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QSize, Qt
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtSvgWidgets import QSvgWidget
 
 from src.router.Route import Route
 from src.utils import globalVars
+from src.utils.ressources import images_path
 
+class ProfilePicture(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(0, 40, 0, 0)
+
+        self.layout = QHBoxLayout()
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setStyleSheet("""
+            QWidget {
+                padding: 0px 0px;
+            }
+        """)
+
+        self.setFixedHeight(200)
+
+        self.picture = QLabel()
+        self.imageProfile = QPixmap()
+
+        self.picture.setStyleSheet("""
+            QWidget {
+                color: #4F23E2;
+                font-weight: 600;
+                font-size: 25px;
+                padding: 0px
+            }
+        """)
+
+        if globalVars.user.picture is None or len(globalVars.user.picture.split(",")) != 2:
+            self.picture = QSvgWidget(images_path("circle_account.svg"))
+            self.picture.setFixedSize(QSize(200, 200))
+        else:
+            self.imageProfile.loadFromData(base64.b64decode(globalVars.user.picture.split(",")[1]))
+            self.imageProfile = self.imageProfile.scaled(200, 200, Qt.AspectRatioMode.IgnoreAspectRatio,
+                                                         Qt.TransformationMode.SmoothTransformation)
+            self.picture.setPixmap(self.imageProfile)
+
+        self.layout.addWidget(self.picture)
+        self.setLayout(self.layout)
 
 class InformationLabel(QtWidgets.QWidget):
     def __init__(self, infotitle="", info=""):
@@ -14,22 +62,10 @@ class InformationLabel(QtWidgets.QWidget):
         )
         self.data = QtWidgets.QLabel(info)
 
-        layout = QtWidgets.QVBoxLayout()
+        layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.data)
         self.setLayout(layout)
-
-
-class ProfilePicture(QtWidgets.QLabel):
-    def __init__(self):
-        super().__init__()
-
-        self.logoImage = QtGui.QPixmap("src/images/logo_schood.png")
-        self.setPixmap(self.logoImage)
-        self.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.setFixedHeight(300)
-        self.setFixedWidth(300)
-
 
 class ProfilePage(Route):
     def __init__(self, parent):
@@ -39,19 +75,19 @@ class ProfilePage(Route):
 
         self.profilepicture = ProfilePicture()
         self.firstname = InformationLabel("First Name:", "")
-        self.firstname.data.setStyleSheet("color: #000000;" "font-size: 22px;")
+        self.firstname.data.setStyleSheet("color: #000000;" "font-size: 22px;" "font-weight: 600;")
         self.lastname = InformationLabel("Last Name:", "")
-        self.lastname.data.setStyleSheet("color: #000000;" "font-size: 22px;")
+        self.lastname.data.setStyleSheet("color: #000000;" "font-size: 22px;" "font-weight: 600;")
         self.classes = InformationLabel("Classe:", "")
-        self.classes.data.setStyleSheet("color: #000000;" "font-size: 22px;")
+        self.classes.data.setStyleSheet("color: #000000;" "font-size: 22px;" "font-weight: 600;")
         self.email = InformationLabel("Email:", "")
-        self.email.data.setStyleSheet("color: #000000;" "font-size: 22px;")
+        self.email.data.setStyleSheet("color: #000000;" "font-size: 22px;" "font-weight: 600;")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QVBoxLayout(self)
         self.layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetMaximumSize)
-        self.layout.setSpacing(24)
-        
+        self.layout.setSpacing(20)
+
         self.layout.addWidget(self.profilepicture, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.firstname, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.firstname.data, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
@@ -61,7 +97,6 @@ class ProfilePage(Route):
         self.layout.addWidget(self.classes.data, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.email, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.email.data, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        # self.layout.setSpacing(24)
         self.layout.addStretch()
         self.setLayout(self.layout)
 
