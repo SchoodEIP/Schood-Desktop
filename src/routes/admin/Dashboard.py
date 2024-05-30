fadcaf7d031dce56a502a0ad131dd77f30f61fb6
Mateo Deroche <mateo.deroche@epitech.eu>
2024-05-30 14:39:07 +0200

    [+] main sentence on all dashboard

--- src/routes/admin/Dashboard.py
+++ src/routes/admin/Dashboard.py
@@ -9,14 +9,31 @@ class AdminDashboard(Route):
         super().__init__()
 
         self.parent = parent
-        self.layout = QtWidgets.QVBoxLayout()
-        self.fullName = QtWidgets.QLabel("")
-        self.text = QtWidgets.QLabel("Admin dashboard")
-        self.layout.setAlignment(QtCore.Qt.AlignCenter)
-        self.layout.addWidget(self.fullName)
-        self.layout.addWidget(self.text)
+        self.mainLayout = QtWidgets.QHBoxLayout()
+        self.mainLayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.mainLayout.setContentsMargins(32, 32, 0, 0)
 
-        self.setLayout(self.layout)
+        self.mainWidget = QtWidgets.QWidget()
+        self.mainWidget.setStyleSheet("color: #000000")
+
+        self.subLayout = QtWidgets.QVBoxLayout()
+        self.subLayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+
+        self.firstname = QtWidgets.QLabel("Bonjour " + stores.user.firstName)
+        self.firstname.setStyleSheet("color: #000000")
+        self.text = QtWidgets.QLabel("Pas de données à afficher pour le moment.")
+        self.text.setStyleSheet("color: #000000")
+        
+        self.title = QtWidgets.QLabel(self.firstname.text())
+        self.title.setStyleSheet("font-size: 32px;" "font-weight: 600;" "color: #000000")
+
+        self.subLayout.addWidget(self.title)
+
+        self.mainWidget.setLayout(self.subLayout)
+        self.mainLayout.addWidget(self.mainWidget)
+        self.mainLayout.addWidget(self.text)
+
+        self.setLayout(self.mainLayout)
 
     def update(self):
-        self.fullName.setText(stores.user.firstName + " " + stores.user.lastName)
+        self.firstname.setText(stores.user.firstName)
